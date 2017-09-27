c947be8934b28edd4c625f5c3a2499b0c5880986
vartoso <vartoso.blusnap@gmail.com>
2017-09-27 21:48:15 +0300

    RC2

--- structures.h
+++ structures.h
@@ -9,37 +9,56 @@
 #ifndef structures_h
 #define structures_h
 
-typedef struct  nodes {
-    int x;
-    int y;
-    int force;
-    int parentx;
-    int parenty;
+typedef struct nodes
+{
+    int  x;
+    int  y;
+    int  force;
+    int  parentx;
+    int  parenty;
     bool closedSet;
+    //stop auto padding warnings
+    char manualMemAligment1;
+    char manualMemAligment2;
+    char manualMemAligment3;
 } node;
 
-typedef struct cords {
-    int x;
-    int y;
-    int force;
+typedef struct cords
+{
+    int  x;
+    int  y;
+    int  force;
     bool closedSet;
+    //stop auto padding warnings
+    //char manualMemAligment1;
+    //char manualMemAligment2;
+    //char manualMemAligment3;
 } cord;
 
-typedef enum {
+typedef enum
+{
     nullPointer,
     noInput,
     noLineScan,
-    closeFail
+    closeFail,
+    badInput
 }errors;
 
-struct errordesc{
-    int  code;
-    char *message;
-} errordesc[] = {
+typedef struct errordesc
+{
+    int    messageNum;
+    char * message;
+} errordescs;
+
+static errordescs errordesc[] =
+{
     { nullPointer, "NULL pointer error" },
     { noInput, "Couldn't read File" },
     { noLineScan, "Couldn't read the line from the file" },
-    { closeFail, "Couldn't close file"}
+    { closeFail, "Couldn't close file"},
+    { badInput, "The format of the input file is wrong, \
+        IT must beggin with an itteration over the X and the the Y"}
 };
+
 
 #endif /* structures_h */
