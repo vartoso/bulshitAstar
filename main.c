4f65bf4b0dafb033c180c54e3724973ff0396411
vartoso <vartoso.blusnap@gmail.com>
2017-09-27 21:48:47 +0300

    CR2

--- main.c
+++ main.c
@@ -10,33 +10,34 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include "preset.h"
-//only then
 #include "structures.h"
-//and only then
-#include "functions.h"
+#include "functions.c"
 
 
 
 
 int main()
 {
+    int matrix[2]; //0 is X and 1 is Y
     cord *map;
     cord start;
     cord end;
     
-    start.x=7;
-    start.y=8;
-    end.x=0;
-    end.y=0;
+    start.x = 7;
+    start.y = 8;
+    end.x   = 0;
+    end.y   = 0;
     
-    map =  (cord *) malloc(sizeof( cord) * 100);
-    if ( ((void *)map == NULL) || ( sizeof map == 0) ){
+    //I'm limiting the input matrix to a 1000 on 1000
+    map = (cord * ) malloc( sizeof(cord) * 1000000);
+    if ( ((void * )map == NULL) || (sizeof map == 0) )
+    {
         error(0);
-        return 1;
     }
-    input(map,"input.txt");
-     cord *ret = aStar(start,end,map);//,nodes);
-    for (int i = sizeof(ret); i>0 ; i--){
+    *matrix = *input( map,"input.txt", matrix);
+    cord *ret = aStar( start, end, map, matrix);//map is map that consists from nodes);
+    for ( int i = sizeof(ret); i > 0; i-- )
+    {
         printf("%d\tx : %d \ty : %d\n", i, ret[i].x , ret[i].y);
     }
     return 0;
